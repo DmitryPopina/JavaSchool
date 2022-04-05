1b8d0b2d213dd7817db44ae9ac5edddd7958d891
Dmitry Popina <d.popina@gmail.com>
2022-04-06 00:50:39 +0300

    Some formatting

--- com/github/dmitrypopina/javaschool/lesson1/Lesson1.java
+++ com/github/dmitrypopina/javaschool/lesson1/Lesson1.java
@@ -9,27 +9,27 @@ class Ex1 {
         int y = 0, cnt = 0;
         System.out.print("Введите целые числа через пробел, до 10 чисел: ");
         while (y < mas.length){
-            if (! console.hasNext()){
+            if (! console.hasNext()) {
                 console.close();
                 break;
             }
             if (console.hasNextInt()) {
                 mas[cnt] = console.nextInt();
                 cnt ++;
-            }else{
+            }else {
                 console.next();
             }
             y++;
         }
 
         System.out.println("Массив до сортировки: ");
-        for (int i = 0; i < cnt; i++){
+        for (int i = 0; i < cnt; i++) {
             System.out.print(mas[i] + " ");
         }
 
-        for (int i = cnt-1; i > 0 ; i--){
+        for (int i = cnt-1; i > 0 ; i--) {
             for (int j = 0; j  < i; j++) {
-                if (mas[j] > mas[j+1]){
+                if (mas[j] > mas[j+1]) {
                     y = mas[j+1];
                     mas[j+1] = mas[j];
                     mas[j] = y;
@@ -38,7 +38,7 @@ class Ex1 {
         }
 
         System.out.print("\nМассив после сортировки: ");
-        for (int i = 0; i < cnt; i++){
+        for (int i = 0; i < cnt; i++) {
             System.out.print(mas[i] + " ");
         }
     }
