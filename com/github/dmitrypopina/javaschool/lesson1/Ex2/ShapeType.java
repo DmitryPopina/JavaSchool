f0156eb4675f24397bd725188df04ef48d4d848c
Dmitry Popina <d.popina@gmail.com>
2022-04-07 01:33:31 +0300

    Advanced interface

--- com/github/dmitrypopina/javaschool/lesson1/Ex2/ShapeType.java
+++ com/github/dmitrypopina/javaschool/lesson1/Ex2/ShapeType.java
@@ -1,8 +1,18 @@
 package com.github.dmitrypopina.javaschool.lesson1.Ex2;
 
 public enum ShapeType {
-    Circle,
-    Triangle,
-    Rect,
-    Square
+    Circle ("Круг"),
+    Triangle ("Треугольник"),
+    Rect ("Прямоугольник"),
+    Square ("Квадрат");
+
+    private final String title;
+    ShapeType(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title;
+    }
 }
