327aaec01227433001bdc52c3d21f23cba6cb2a9
Dmitry Popina <d.popina@gmail.com>
2022-04-13 15:54:20 +0300

    Made some enhancements and bugfixes

---
+++ com/github/dmitrypopina/javaschool/lesson3/Ex1/CountMapImpl.java
@@ -0,0 +1,46 @@
+package com.github.dmitrypopina.javaschool.lesson3.Ex1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountMapImpl<T> implements CountMap<T> {
+    private final HashMap<T, Integer> map = new HashMap<>();
+    @Override
+    public void add(T o) {
+        map.merge(o, 1, (oldValue, newValue) -> ++oldValue);
+    }
+
+    @Override
+    public int getCount(T o) {
+        return map.getOrDefault(o,0);
+    }
+
+    @Override
+    public int remove(T o) {
+        int cnt = map.getOrDefault(o, 0);
+        if (cnt > 0 ) {
+            map.remove(o);
+        }
+        return cnt;
+    }
+
+    @Override
+    public int size() {
+        return map.keySet().size();
+    }
+
+    @Override
+    public void addAll(CountMap<T> source) {
+        source.toMap().forEach((key, value) -> map.merge(key, value, Integer::sum));
+    }
+
+    @Override
+    public Map<T, Integer> toMap() {
+        return map;
+    }
+
+    @Override
+    public void toMap(Map<T, Integer> destination) {
+        destination.putAll(map);
+    }
+}
---
+++ com/github/dmitrypopina/javaschool/lesson3/Ex1/Ex1.java
@@ -0,0 +1,52 @@
+package com.github.dmitrypopina.javaschool.lesson3.Ex1;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        CountMap<Integer> map = new CountMapImpl<>();
+
+        map.add(10);
+        map.add(10);
+        map.add(5);
+        map.add(6);
+        map.add(5);
+        map.add(10);
+
+        System.out.println("Первоначальный список:");
+        map.toMap().forEach((key, value) -> System.out.println("Count " + key + " " + value));
+
+        System.out.println("Запросы из задания:");
+        int count;
+        count = map.getCount(5); // 2
+        System.out.println("Count 5 " + count);
+        count = map.getCount(6); // 1
+        System.out.println("Count 6 " + count);
+        count = map.getCount(10); // 3
+        System.out.println("Count 10 " + count);
+
+        CountMap<Integer> nmap = new CountMapImpl<>();
+        nmap.add(10);
+        nmap.add(10);
+        nmap.add(5);
+        nmap.add(6);
+        nmap.add(5);
+        nmap.add(10);
+
+        System.out.println("Список после удвоения:");
+        map.addAll(nmap);
+        map.toMap().forEach((key, value) -> System.out.println("Count " + key + " " + value));
+
+
+        count = map.remove(10);
+        System.out.println("Count 10 до удаления " + count);
+        count = map.remove(5);
+        System.out.println("Count 5 до удаления " + count);
+        count = map.remove(6);
+        System.out.println("Count 6 до удаления " + count);
+        count = map.remove(6);
+        System.out.println("Count 6 до удаления " + count);
+
+        System.out.println("Список после удаления:");
+        map.toMap().forEach((key, value) -> System.out.println("Count " + key + " " + value));
+
+    }
+}
