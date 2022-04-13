84b53a24671b57cdb654230e4e258829ced47d7b
Dmitry Popina <d.popina@gmail.com>
2022-04-13 23:17:10 +0300

    Some fixes

--- com/github/dmitrypopina/javaschool/lesson2/Ex2/Ex2.java
+++ com/github/dmitrypopina/javaschool/lesson2/Ex2/Ex2.java
@@ -39,6 +39,7 @@ public class Ex2 {
             System.out.println("Количнство различных слов в файле  " + wordCatalog.keySet().size());
 
             //Задание 2: Выведите на экран список различных слов файла, отсортированный по возрастанию их длины (компаратор сначала по длине слова, потом по тексту).
+            //Вместо компаратора реализовал compareTo.
             System.out.println("\n<<Решение 2: Выведите на экран список различных слов файла, отсортированный по возрастанию их длины (компаратор сначала по длине слова, потом по тексту)>>");
             for (FileWord wrd: tree) {
                 System.out.println(wrd.getWord());
@@ -52,10 +53,16 @@ public class Ex2 {
             }
 
             //Задание 4: Выведите на экран все строки файла в обратном порядке
+            System.out.println("\n<<Решение 4: Выведите на экран все строки файла в обратном порядке>>");
+            TreeSet<String> tStrings = new TreeSet<>(lines);
+            Iterator<String> backStringIterator = tStrings.descendingIterator();
+            while (backStringIterator.hasNext()) {
+                System.out.println(backStringIterator.next());
+            }
             //Задание 5: Реализуйте свой Iterator для обхода списка в обратном порядке.
-            System.out.println("\n<<Решение 4-5: Выведите на экран все строки файла в обратном порядке>>");
+            System.out.println("\n<<Решение 5: Реализуйте свой Iterator для обхода списка в обратном порядке>>");
             FileStrings fStrings = new FileStrings(lines);
-            Iterator<String> backStringIterator = fStrings.backiterator();
+            backStringIterator = fStrings.backiterator();
             while (backStringIterator.hasNext()) {
                 System.out.println(backStringIterator.next());
             }
